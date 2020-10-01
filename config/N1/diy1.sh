672ab0145e2a08f7eda84b169e0992409abbc366
ylqjgm <ylqjgm@gmail.com>
2020-10-02 00:46:05 +0800

    fix(filebrowser): 修复luci-app-filebrowser不安装的问题

--- config/N1/diy1.sh
+++ config/N1/diy1.sh
@@ -8,5 +8,9 @@ svn co https://github.com/rosywrt/luci-theme-rosy/trunk/luci-theme-rosy package/
 # 低调上网
 sed -i 's/#src-git helloworld/src-git helloworld/g' feeds.conf.default
 
+# passwall
 svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/lean/luci-app-passwall
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/lean/package
+
+# filebrowser
+svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-filebrowser package/lean/luci-app-filebrowser
