01318bf7481ec21b1bed42121947ea23e7ea108e
ylqjgm <ylqjgm@gmail.com>
2020-10-03 19:06:48 +0800

    fix(mknop更名): mknop下执行文件更名，同步更新

--- config/N1/organizer.sh
+++ config/N1/organizer.sh
@@ -5,5 +5,5 @@ echo "Build Firmware"
 git clone https://github.com/tuanqing/mknop.git mknop
 cp -r -f dist/openwrt-armvirt-64-default-rootfs.tar.gz mknop/openwrt/
 cd mknop
-sudo ./make -d
+sudo ./gen_openwrt -d
 mv out/*/*.img ../dist/
