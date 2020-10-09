4142eec58bb18ae3c2467eb4e405f2947fe9c1a7
ylqjgm <ylqjgm@gmail.com>
2020-10-10 01:34:55 +0800

    fix(mknop): 修复mknop打包及其他一些小调整

--- config/N1/diy2.sh
+++ config/N1/diy2.sh
@@ -4,13 +4,3 @@
 sed -i 's#192.168.1.1#192.168.1.3#g' package/base-files/files/bin/config_generate
 # 替换默认主题
 sed -i 's#luci-theme-bootstrap#luci-theme-rosy#g' feeds/luci/collections/luci/Makefile
-
-# 更新GoLang到1.15.2
-sed -i 's#GO_VERSION_MAJOR_MINOR:=1.14#GO_VERSION_MAJOR_MINOR:=1.15#g' feeds/packages/lang/golang/golang/Makefile
-sed -i 's#GO_VERSION_PATCH:=4#GO_VERSION_PATCH:=2#g' feeds/packages/lang/golang/golang/Makefile
-sed -i 's#PKG_HASH:=7011af3bbc2ac108d1b82ea8abb87b2e63f78844f0259be20cde4d42c5c40584#PKG_HASH:=28bf9d0bcde251011caae230a4a05d917b172ea203f2a62f2c2f9533589d4b4d#g' feeds/packages/lang/golang/golang/Makefile
-
-# 更新V2Ray到2.29.0
-sed -i 's#PKG_VERSION:=4.28.2#PKG_VERSION:=4.29.0#g' package/lean/v2ray/Makefile
-sed -i 's#PKG_HASH:=e78446333fd28ef8a3ec2c25612458bfc024a226008888ed5277201136f4ca97#PKG_HASH:=cc8cb22b616c218a6de9de0bee80651f502344a907b696008ae7bd467ba05a17#g' package/lean/v2ray/Makefile
-sed -i 's#https://github.com/v2ray#https://github.com/v2fly#g' package/lean/v2ray/Makefile
--- config/N1/organizer.sh
+++ config/N1/organizer.sh
@@ -2,8 +2,8 @@
 
 echo "Build Firmware"
 
-git clone https://github.com/tuanqing/mknop.git mknop
+git clone https://github.com/ylqjgm/mknop.git mknop
 cp -r -f dist/openwrt-armvirt-64-default-rootfs.tar.gz mknop/openwrt/
 cd mknop
-sudo ./gen_openwrt -d -m phicomm-n1
+sudo ./gen_openwrt -d -k 5.4.69 -m phicomm-n1
 mv out/*/*.img ../dist/
