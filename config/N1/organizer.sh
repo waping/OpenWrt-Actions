ac8ae45fb99fc59f1d32f77e58abf0d5445ddd6f
ylqjgm <ylqjgm@gmail.com>
2020-10-10 11:56:05 +0800

    fix(build): 修复mknop没有默认openwrt目录问题，启用github新语法

--- config/N1/organizer.sh
+++ config/N1/organizer.sh
@@ -3,7 +3,8 @@
 echo "Build Firmware"
 
 git clone https://github.com/ylqjgm/mknop.git mknop
+mkdir -p mknop/openwrt
 cp -r -f dist/openwrt-armvirt-64-default-rootfs.tar.gz mknop/openwrt/
 cd mknop
 sudo ./gen_openwrt -d -k 5.4.69 -m phicomm-n1
-mv out/*/*.img ../dist/
+mv out/phicomm-n1/*.img ../dist/
