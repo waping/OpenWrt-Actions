00f90132364f8487d1807559a978c20102011d52
waping <74528234+waping@users.noreply.github.com>
2020-11-17 09:57:50 +0800

    Update diy1.sh

--- config/K2P/diy1.sh
+++ config/K2P/diy1.sh
@@ -2,3 +2,7 @@
 
 # 下载主题
 svn co https://github.com/rosywrt/luci-theme-rosy/trunk/luci-theme-rosy package/luci-theme-rosy
+
+# passwall
+svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/lean/luci-app-passwall
+svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/lean/package
